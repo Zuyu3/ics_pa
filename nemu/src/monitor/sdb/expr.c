c19fe46c70ec77c47b1808df4b6fd7e8c33e4dc6
tracer-ics2022 <tracer@njuics.org>
2022-09-19 16:53:29 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:53:29 up  2:18,  1 user,  load average: 0.10, 0.21, 0.22

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -1,17 +1,17 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include <isa.h>
 
@@ -20,33 +20,36 @@
  */
 #include <regex.h>
 
-enum {
-  TK_NOTYPE = 256, TK_EQ, TK_DECNUM
+enum
+{
+  TK_NOTYPE = 256,
+  TK_EQ,
+  TK_DECNUM
 
   /* TODO: Add more token types */
 
 };
 
-static struct rule {
+static struct rule
+{
   const char *regex;
   int token_type;
 } rules[] = {
 
-  /* TODO: Add more rules.
-   * Pay attention to the precedence level of different rules.
-   */
+    /* TODO: Add more rules.
+     * Pay attention to the precedence level of different rules.
+     */
 
-  {" +", TK_NOTYPE},    // spaces
-  {"\\+", '+'},         // plus
-  {"==", TK_EQ},        // equal
-  {"\\-", '-'},
-  {"\\*", '*'},
-  {"\\/", '/'},
-  {"\\(", '('},
-  {"\\)", ')'},
-  {"[0-9]+", TK_DECNUM},
-  
-  
+    {" +", TK_NOTYPE}, // spaces
+    {"\\+", '+'},      // plus
+    {"==", TK_EQ},     // equal
+    {"\\-", '-'},
+    {"\\*", '*'},
+    {"\\/", '/'},
+    {"\\(", '('},
+    {"\\)", ')'},
+    {"[0-9]+", TK_DECNUM},
+
 };
 
 #define NR_REGEX ARRLEN(rules)
@@ -56,39 +59,47 @@ static regex_t re[NR_REGEX] = {};
 /* Rules are used for many times.
  * Therefore we compile them only once before any usage.
  */
-void init_regex() {
+void init_regex()
+{
   int i;
   char error_msg[128];
   int ret;
 
-  for (i = 0; i < NR_REGEX; i ++) {
+  for (i = 0; i < NR_REGEX; i++)
+  {
     ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
-    if (ret != 0) {
+    if (ret != 0)
+    {
       regerror(ret, &re[i], error_msg, 128);
       panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
     }
   }
 }
 
-typedef struct token {
+typedef struct token
+{
   int type;
   char str[32];
 } Token;
 
 static Token tokens[32] __attribute__((used)) = {};
-static int nr_token __attribute__((used))  = 0;
+static int nr_token __attribute__((used)) = 0;
 
-static bool make_token(char *e) {
+static bool make_token(char *e)
+{
   int position = 0;
   int i;
   regmatch_t pmatch;
 
   nr_token = 0;
 
-  while (e[position] != '\0') {
+  while (e[position] != '\0')
+  {
     /* Try all rules one by one. */
-    for (i = 0; i < NR_REGEX; i ++) {
-      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+    for (i = 0; i < NR_REGEX; i++)
+    {
+      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0)
+      {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
@@ -101,9 +112,9 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-        
 
-        if(substr_len > 32){
+        if (substr_len > 32)
+        {
           printf("Substr is too long.\n");
           assert(0);
         }
@@ -114,7 +125,8 @@ static bool make_token(char *e) {
           break;
         default:
           tokens[nr_token].type = rules[i].token_type;
-          for (int j = 0; j < substr_len; j++) {
+          for (int j = 0; j < substr_len; j++)
+          {
             tokens[nr_token].str[j] = substr_start[j];
           }
           printf("%d    %s\n", tokens[nr_token].type, tokens[nr_token].str);
@@ -124,7 +136,8 @@ static bool make_token(char *e) {
       }
     }
 
-    if (i == NR_REGEX) {
+    if (i == NR_REGEX)
+    {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
     }
@@ -133,50 +146,130 @@ static bool make_token(char *e) {
   return true;
 }
 
-bool check_parentheses(int p, int q){
+bool check_parentheses(int p, int q)
+{
   /*check if the expression is surrounded by a matched pair of parentheses.
    *return true if it is.
-  */
-  //TODO: implement this function later.
-  return true;
+   */
+  // TODO: check it later.
+  if (tokens[p].type == '(' && tokens[q].type == ')')
+    return true;
+  return false;
 }
 
-word_t eval(int p, int q) {
-  if (p > q) {
-    //Bad expression.
-    //TODO:maybe need further check here.
-    return 0;
-  }
-  else if (p == q) {
-    // Single token.
-    // For now this token should be a number.
-    // Return the value of the number.
-    //TODO: implement Hex numbers here.
-    word_t res;
-    sscanf(tokens[p].str, "%d", &res);
-    return res;
-  }
-  else if (check_parentheses(p, q) == true) {
-    /* The expression is surrounded by a matched pair of parentheses.
-     * If that is the case, just throw away the parentheses.
-     */
-    return eval(p + 1, q - 1);
-  }
-  else {
-    /* We should do more things here. */
-    //TODO: implement it here.
-    return 0;
-  }
-}
-
-word_t expr(char *e, bool *success) {
-  if (!make_token(e)) {
+word_t eval(int p, int q, bool *success)
+{
+  word_t res;
+  if (p > q)
+  {
+    // Bad expression.
+    // TODO:maybe need further check here.
     *success = false;
     return 0;
   }
+  else if (p == q)
+  {
+    // Single token.
+    // For now this token should be a number.
+    // Return the value of the number.
+    sscanf(tokens[p].str, "%d", &res);
+    return res;
+  }
+  else if (check_parentheses(p, q) == true)
+  {
+    /* The expression is surrounded by a matched pair of parentheses.
+     * If that is the case, just throw away the parentheses.
+     */
+    return eval(p + 1, q - 1, success);
+  }
+  else
+  {
+    // TODO: implement it here.
+    // first scan for '+' || '-'
+    for (int i = p + 1, parenthesesCounter = 0; i < q; i++)
+    {
+      switch (tokens[i].type)
+      {
+      case '+':
+        if (!parenthesesCounter)
+        {
+          res = eval(p, i - 1, success) + eval(i + 1, q, success);
+          return *success ? res : 0;
+        }
+        break;
+      case '-':
+        if (!parenthesesCounter)
+        {
+          res = eval(p, i - 1, success) - eval(i + 1, q, success);
+          return *success ? res : 0;
+        }
+        break;
+      case '(':
+        parenthesesCounter++;
+        break;
+      case ')':
+        if (parenthesesCounter > 0)
+          parenthesesCounter--;
+        else
+        {
+          *success = false;
+          return 0;
+        }
+        break;
+      default:
+        break;
+      }
+    }
 
-  /* TODO: Insert codes to evaluate the expression. */
-  TODO();
+    // second scan for '*' || '/'
+    for (int i = p + 1, parenthesesCounter = 0; i < q; i++)
+    {
+      switch (tokens[i].type)
+      {
+      case '*':
+        if (!parenthesesCounter)
+        {
+          res = eval(p, i - 1, success) * eval(i + 1, q, success);
+          return *success ? res : 0;
+        }
+        break;
+      case '/':
+        if (!parenthesesCounter)
+        {
+          res = eval(p, i - 1, success) / eval(i + 1, q, success);
+          return *success ? res : 0;
+        }
+        break;
+      case '(':
+        parenthesesCounter++;
+        break;
+      case ')':
+        if (parenthesesCounter > 0)
+          parenthesesCounter--;
+        else
+        {
+          *success = false;
+          return 0;
+        }
+        break;
+      default:
+        break;
+      }
+    }
+    *success = false;
+    // TODO:cause an error.
+    return 0;
+  }
+}
 
-  return 0;
+word_t expr(char *e, bool *success)
+{
+  if (!make_token(e))
+  {
+    *success = false;
+    return 0;
+  }
+  *success = true;
+  // TODO: maybe need to check success here.
+  return eval(0, nr_token, success);
 }
