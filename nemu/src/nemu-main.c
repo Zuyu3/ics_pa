304404f042f3c25bb887dc986d33261a8bf86433
tracer-ics2022 <tracer@njuics.org>
2022-09-20 14:21:18 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:21:18 up 25 min,  1 user,  load average: 0.74, 0.58, 0.48

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -45,6 +45,7 @@ int main(int argc, char *argv[]) {
       assert(0);
     }
     printf("%d\n%s\n", stdAnswer, toEvalExpr);
+    printf("%c   %c", toEvalExpr[267], toEvalExpr[268]);
     myAnswer = expr(toEvalExpr, &s);
     if(myAnswer != stdAnswer){
       printf("WA!\ninput expression is: %s\n Standard is %d, but my answer is %d\n",toEvalExpr, stdAnswer, myAnswer);
