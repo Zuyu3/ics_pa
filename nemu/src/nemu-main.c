30475e577aa24ad5bd6ea109e4ec05b4bf17a2c6
tracer-ics2022 <tracer@njuics.org>
2022-09-21 09:30:55 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:30:55 up 8 min,  1 user,  load average: 0.37, 0.32, 0.22

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
   init_monitor(argc, argv);
 #endif
 
-
+  /*Test Module
   FILE *fp = fopen("tools/gen-expr/input", "r");
   char toEvalExpr[50000];
   unsigned int stdAnswer, myAnswer;
@@ -52,6 +52,9 @@ int main(int argc, char *argv[]) {
       printf("Pass expression %d\n\n", i + 1);
     }
   }
+  */
+
+ 
   /* Start engine. */
   engine_start();
 
