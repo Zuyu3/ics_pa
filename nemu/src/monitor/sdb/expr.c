ba36889dbaf4545c6cedfa8427046346ab43fcfb
tracer-ics2022 <tracer@njuics.org>
2022-09-22 08:42:01 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:42:01 up 39 min,  1 user,  load average: 1.03, 0.70, 0.56

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -63,6 +63,7 @@ static struct rule
     {"[0-9]+u", TK_DECNUMU},
     {"[0-9]+", TK_DECNUM},
     {"\\$(0|ra|[sgt]p|t[0-6]|a[0-7]|s[0-9]|s10|s11)", TK_REGISTER},
+    {"\\$.{1,2}", TK_REGISTER}
     //$0, ra, sp, gp, tp, t0, t1, t2, s0, s1, a0, a1, a2, a3, a4, a5, a6, a7, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, t3, t4, t5, t6
     
 };
@@ -247,9 +248,9 @@ word_t eval(int p, int q, bool *success)
       *success = sscanf(tokens[p].str, "%d", &res);
     else if (tokens[p].type == TK_DECNUM)
       *success = sscanf(tokens[p].str, "%x", &res);
-    else {
-      //TODO:
-      return 0;
+    else if (tokens[p].type == TK_REGISTER) {
+      res = isa_reg_str2val("$0", success);
+      return *success ? res: 0; 
     }
     return *success? res: 0;
   }
