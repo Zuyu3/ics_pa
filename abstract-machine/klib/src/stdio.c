dba8f5157263e578053dc76146390482ecf8f036
tracer-ics2022 <tracer@njuics.org>
2022-10-18 16:23:07 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:23:07 up 57 min,  1 user,  load average: 0.13, 0.15, 0.11

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,6 +5,34 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+int format_helper_int2str(char *out, int d) {
+  int res = 0;
+  if(d < 0) {
+      *out = '-';
+      out++;
+      if(d == 0x80000000) {
+          strcpy(out, "2147483648");
+          return 11;
+      }
+      return 1 + format_helper_int2str(out, -d);
+  }
+  char temp;
+  do {
+      *(out + res) = d % 10  + '0';
+      res++;
+      d /= 10;
+  } while(d);
+
+  *(out + res) = '\0';
+
+  for(int i = 0; i < res / 2; i++) {
+      temp = *(out + i);
+      *(out + i) = *(out + res - i - 1);
+      *(out + res - i - 1) = temp;
+  }
+  return res;
+}
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -14,7 +42,39 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  int res = 0;
+  va_list ap;
+  va_start(ap, fmt);
+
+  while(*fmt != '\0') {
+      if(*fmt == '%') {
+          switch (*(++fmt))
+          {
+          case 'd':
+              res += format_helper_int2str(out + res, va_arg(ap, int));
+              break;
+          case 's':
+              strcpy(out + res, va_arg(ap, char*));
+              res += strlen(out + res);
+              break;
+          case 'c':
+              *(out + res) = va_arg(ap, int);
+              res++;
+              break;
+          default:
+              break;
+          }
+      }
+      else {
+          *(out + res) = *fmt;
+          res++;
+      }
+      fmt++;
+  }
+  va_end(ap);
+  *(out + res) = '\0';
+  return res;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,7 +5,7 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  panic("Not tested");
   size_t res = 0;
   while(*s != '\0'){
     res++;
@@ -26,7 +26,20 @@ char *strcpy(char *dst, const char *src) {
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  panic("Not tested");
+  char *end_of_dst = dst;
+  while(n--) {
+    if(*src == '\0')
+      break;
+    *end_of_dst = *src;
+    end_of_dst++;
+    src++;
+  }
+  while(n--) {
+    *end_of_dst = '\0';
+    end_of_dst++;
+  }
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
@@ -57,7 +70,14 @@ int strcmp(const char *s1, const char *s2) {
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  panic("Not tested");
+  while(n--) {
+    if(*s1 != *s2 || *s1 == '\0')
+      return *s2 - *s2;
+    s1++;
+    s2++;
+  }
+  return 0;
 }
 
 void *memset(void *s, int c, size_t n) {
@@ -70,11 +90,31 @@ void *memset(void *s, int c, size_t n) {
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  panic("Not tested");
+  char src_cpy[n];
+  void *end_of_dst = dst;
+
+  for(int i = 0; i < n; i++) {
+    src_cpy[i] = *(char*) src;
+    src++;
+  }
+
+  for(int i = 0; i < n; i++) {
+    *(char*)end_of_dst = src_cpy[i];
+    end_of_dst++;
+  }
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  panic("Not tested");
+  void *end_of_out = out;
+  while(n--) {
+    *(char *)end_of_out = *(char *)in;
+    end_of_out++;
+    in++;
+  }
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
