a959eda2ced3c029659829d548a8a71e1388efb3
tracer-ics2022 <tracer@njuics.org>
2022-10-19 11:49:39 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:49:39 up  1:00,  1 user,  load average: 0.55, 0.49, 0.41

--- nemu/src/cpu/iringbuf.c
+++ nemu/src/cpu/iringbuf.c
@@ -24,7 +24,7 @@ void print_ibuf_log() {
   printf("Here are the %d most recent instructions executed before the program error\n", IBUF_SIZE);
   for(int i = (iringbuf_index + 1) % IBUF_SIZE; i != iringbuf_index; i = (i + 1) % IBUF_SIZE) {
     if(!strcmp(iringbuf[i], ""))
-    continue;
+      continue;
     printf("%s\n", iringbuf[i]);
   }
 }
