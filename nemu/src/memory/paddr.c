d08527b9a22a7497c1ff31100ed9c56a99ca28b7
tracer-ics2022 <tracer@njuics.org>
2022-10-19 17:27:02 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:27:02 up  1:10,  1 user,  load average: 0.69, 0.49, 0.47

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -41,6 +41,7 @@ static void pmem_write(paddr_t addr, int len, word_t data) {
 
 static void out_of_bound(paddr_t addr) {
   print_ibuf_log();
+  printf("\n\n\n");
   print_mbuf_log();
   panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
       addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
