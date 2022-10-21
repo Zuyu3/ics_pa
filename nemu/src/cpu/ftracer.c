735f0a6caf6d5284c4435e86403b6887f8532925
tracer-ics2022 <tracer@njuics.org>
2022-10-21 15:19:33 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:19:33 up  1:17,  1 user,  load average: 0.09, 0.37, 0.41

--- nemu/src/cpu/ftracer.c
+++ nemu/src/cpu/ftracer.c
@@ -178,13 +178,9 @@ void check_func_log(vaddr_t target_addr, vaddr_t curr_addr) {
     #ifndef CONFIG_FTRACE
         return;
     #endif
-    printf("%x %x\n", curr_addr, target_addr);
-    if(func_tracer_index)
-    printf("%x %x\n\n", func_tracer_buf[func_tracer_index - 1]->func_start_addr, func_tracer_buf[func_tracer_index - 1]->func_end_addr);
-    getchar();
 
 
-    if(func_tracer_index != 0 && target_addr > func_tracer_buf[func_tracer_index - 1]->func_start_addr && target_addr <= func_tracer_buf[func_tracer_index - 1]->func_end_addr) {
+    if(func_tracer_index > 1 && target_addr > func_tracer_buf[func_tracer_index - 2]->func_start_addr && target_addr <= func_tracer_buf[func_tracer_index - 2]->func_end_addr) {
         for(int i = 0; i < func_tracer_index; i++)
            fprintf(fp, "  ");
         fprintf(fp, "ret  [%s]\n", func_tracer_buf[func_tracer_index - 1]->func_name);
