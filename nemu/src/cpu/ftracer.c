d9e999eb04fbaef197a19e8053a612d016588586
tracer-ics2022 <tracer@njuics.org>
2022-10-21 17:19:41 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:19:41 up  3:17,  1 user,  load average: 0.22, 0.33, 0.36

--- nemu/src/cpu/ftracer.c
+++ nemu/src/cpu/ftracer.c
@@ -183,7 +183,7 @@ void check_func_log(vaddr_t target_addr, vaddr_t curr_addr) {
 
     //printf("%x %x\n", curr_addr, target_addr);
     for(int i = func_tracer_index - 1; i >= 0; i--) 
-        if(target_addr > func_tracer_buf[i]->func_start_addr && target_addr < func_tracer_buf[i]->func_end_addr) {
+        if(target_addr > func_tracer_buf[i]->func_start_addr && target_addr <= func_tracer_buf[i]->func_end_addr) {
             fprintf(fp, "0x%08x:", curr_addr);
 
             for(int k = 0; k < func_tracer_index; k++)
