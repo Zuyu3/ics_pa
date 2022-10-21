62619ac9be178a4257b9e3a3b5be4d6010df2c45
tracer-ics2022 <tracer@njuics.org>
2022-10-21 11:26:47 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:26:47 up  2:04,  1 user,  load average: 0.16, 0.19, 0.18

--- nemu/src/cpu/ftracer.c
+++ nemu/src/cpu/ftracer.c
@@ -136,12 +136,12 @@ void init_func_table(char *bin_path) {
 
     fp = fopen(elf_path, "r");
     reset_fp_offset();
-    printf("%s\n", elf_path);
+    //printf("%s\n", elf_path);
     load_elf_header(elf_header);
-    printf("%u\n%u\n%u\n%u\n", elf_header->e_shoff, elf_header->e_shentsize, elf_header->e_shnum, elf_header->e_shstrndx);
+    //printf("%u\n%u\n%u\n%u\n", elf_header->e_shoff, elf_header->e_shentsize, elf_header->e_shnum, elf_header->e_shstrndx);
 
     load_section_header();
-    for(int i = 0; i < elf_header->e_shnum; i++)  printf("%-10x  %-10x  %-10x %s\n", sh_data[i].sh_name_offset, sh_data[i].sh_offset, sh_data[i].sh_size, sh_data[i].sh_name);
+    //for(int i = 0; i < elf_header->e_shnum; i++)  printf("%-10x  %-10x  %-10x %s\n", sh_data[i].sh_name_offset, sh_data[i].sh_offset, sh_data[i].sh_size, sh_data[i].sh_name);
 
     for(int i = 0; i < elf_header->e_shnum; i++) {
         if(!strcmp(sh_data[i].sh_name, ".symtab")) {
