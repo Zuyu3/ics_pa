6c661e9ea6bdf039fe6dcd4e452512d7077db4f2
tracer-ics2022 <tracer@njuics.org>
2022-10-21 11:06:16 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:06:16 up  1:43,  1 user,  load average: 0.16, 0.10, 0.18

---
+++ nemu/src/cpu/ftracer.c
@@ -0,0 +1,156 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+char elf_path[] = "/home/zuyu/code/ics2022/am-kernels/tests/am-tests/build/amtest-riscv32-nemu.elf";
+FILE *fp = NULL;
+unsigned int current_offset = 0;
+
+typedef struct {
+    unsigned int e_shoff;
+    unsigned int e_shentsize, e_shnum, e_shstrndx;
+} ElfHeader;
+
+
+typedef struct {
+    unsigned int sh_name_offset, sh_offset, sh_size;
+    char sh_name[50];
+} Section_Header_Data;
+
+typedef struct
+{
+    unsigned int name_offset, func_addr;
+    char func_name[100];
+} Function_Table;
+
+ElfHeader *elf_header = NULL;
+Section_Header_Data *sh_data = NULL;
+Function_Table *func_table = NULL;
+int func_table_size = 0;
+
+void reset_fp_offset() {
+    rewind(fp);
+    current_offset = 0;
+}
+
+void load_data_from_elf(unsigned int offset, int size, void *pointer) {
+    if(offset < current_offset)
+        reset_fp_offset();
+
+    for(int i = current_offset; i < offset; i++) {
+        fgetc(fp);
+    }
+    for(int i = 0; i < size; i++) {
+        *(char *)(pointer + i) = fgetc(fp);
+    }
+    current_offset = offset + size;
+}
+
+void load_string_from_elf(unsigned int offset, char *pointer) {
+    if(offset < current_offset)
+        reset_fp_offset();
+    
+    for(int i = current_offset; i < offset; i++) {
+        fgetc(fp);
+    }
+    
+    int counter = 0;
+    do {
+        pointer[counter] = fgetc(fp);
+    } while(pointer[counter++] != '\0');
+
+    current_offset = offset + counter;
+
+}
+
+void load_elf_header() {
+    elf_header = (ElfHeader *)malloc(sizeof(ElfHeader));
+    load_data_from_elf(32, 4, &elf_header->e_shoff);
+    load_data_from_elf(46, 2, &elf_header->e_shentsize);
+    load_data_from_elf(48, 2, &elf_header->e_shnum);
+    load_data_from_elf(50, 2, &elf_header->e_shstrndx);
+}
+
+void load_section_header() {
+    sh_data = (Section_Header_Data *)malloc(sizeof(Section_Header_Data) * elf_header->e_shnum);
+    for(int i = 0; i < elf_header->e_shnum; i++) {
+        load_data_from_elf(elf_header->e_shoff + elf_header->e_shentsize * i     , 4, &sh_data[i].sh_name_offset);
+        load_data_from_elf(elf_header->e_shoff + elf_header->e_shentsize * i + 16, 4, &sh_data[i].sh_offset);
+        load_data_from_elf(elf_header->e_shoff + elf_header->e_shentsize * i + 20, 4, &sh_data[i].sh_size);
+    }
+
+    for(int i = 0; i < elf_header->e_shnum; i++) {
+        load_string_from_elf(sh_data[elf_header->e_shstrndx].sh_offset + sh_data[i].sh_name_offset, sh_data[i].sh_name);
+    }
+}
+
+void resize_function_table(unsigned int new_size) {
+    Function_Table *temp = (Function_Table *)malloc(sizeof(Function_Table) * new_size);
+    for(int i = 0; i < func_table_size; i++) {
+        memcpy(&temp[i], &func_table[i], sizeof(Function_Table));
+    }
+    free(func_table);
+    func_table = temp;
+}
+
+void load_func_symtab_data(Section_Header_Data *symtab_section_header) {
+    unsigned int sh_part_start = symtab_section_header->sh_offset;
+
+    unsigned int temp_name_offset, temp_addr;
+    unsigned char temp_sym_type;
+    unsigned int ft_size = 50;
+
+    resize_function_table(ft_size);
+
+    for(int i = 0; i < symtab_section_header->sh_size / 16; i++) {
+        load_data_from_elf(sh_part_start     , 4, &temp_name_offset);
+        load_data_from_elf(sh_part_start + 4 , 4, &temp_addr);
+        load_data_from_elf(sh_part_start + 12, 1, &temp_sym_type);
+        temp_sym_type %= 16;
+        sh_part_start += 16;
+
+        if(temp_sym_type == 2) {
+            func_table[func_table_size].name_offset = temp_name_offset;
+            func_table[func_table_size].func_addr = temp_addr;
+            func_table_size++;
+            if(func_table_size == ft_size) {
+                ft_size *= 2;
+                resize_function_table(ft_size);
+            }
+        }
+    }
+}
+
+void load_func_strtab_name(unsigned int strtab_start_addr) {
+    for(int i = 0; i < func_table_size; i++) {
+        load_string_from_elf(strtab_start_addr + func_table[i].name_offset, func_table[i].func_name);
+    }
+}
+
+void init_func_table(char *elf_path) {
+    fp = fopen(elf_path, "r");
+    reset_fp_offset();
+    printf("%s\n", elf_path);
+    load_elf_header(elf_header);
+    //printf("%u\n%u\n%u\n%u\n", elf_header->e_shoff, elf_header->e_shentsize, elf_header->e_shnum, elf_header->e_shstrndx);
+
+    load_section_header();
+    //for(int i = 0; i < elf_header->e_shnum; i++)  printf("%-10x  %-10x  %-10x %s\n", sh_data[i].sh_name_offset, sh_data[i].sh_offset, sh_data[i].sh_size, sh_data[i].sh_name);
+
+    for(int i = 0; i < elf_header->e_shnum; i++) {
+        if(!strcmp(sh_data[i].sh_name, ".symtab")) {
+            load_func_symtab_data(&sh_data[i]);
+        }
+        else if(!strcmp(sh_data[i].sh_name, ".strtab")) {
+            load_func_strtab_name(sh_data[i].sh_offset);
+        }
+    }
+
+
+    free(elf_header);
+    free(sh_data); 
+    for(int i = 0; i < func_table_size; i++)
+    printf("%x  %x %s\n", func_table[i].name_offset, func_table[i].func_addr, func_table[i].func_name);
+    printf("%d\n", func_table_size);
+    getchar();
+}
