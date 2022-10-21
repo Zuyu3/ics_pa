9b85d3ec9b932cef0611a0d59962b9c6c09a7cbb
tracer-ics2022 <tracer@njuics.org>
2022-10-21 21:05:03 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:05:03 up 46 min,  1 user,  load average: 0.58, 0.37, 0.25

--- nemu/src/cpu/ftracer.c
+++ nemu/src/cpu/ftracer.c
@@ -182,7 +182,7 @@ void check_func_log(vaddr_t target_addr, vaddr_t curr_addr) {
     #endif
 
     printf("%x %x\n", curr_addr, target_addr);
-    for(int i = func_tracer_index - 1; i >= 0; i--) {
+    for(int i = func_tracer_index - 2; i >= 0; i--) {
         if(target_addr > func_tracer_buf[i]->func_start_addr && target_addr < func_tracer_buf[i]->func_end_addr) {
             fprintf(fp, "0x%08x:", curr_addr);
 
@@ -190,10 +190,10 @@ void check_func_log(vaddr_t target_addr, vaddr_t curr_addr) {
                 fprintf(fp, "   ");
                     
             
-            for(int k = 0; k < func_table_size; k++) {
-                if(curr_addr >= func_table[k].func_start_addr && curr_addr < func_table[k].func_end_addr) {
-                    fprintf(fp, " ret  [%s]\n", func_table[k].func_name);
-                    printf("ret  [%s] to [%s]\n", func_table[k].func_name, func_tracer_buf[i]->func_name);
+            for(int k = func_tracer_index - 1; k >= 0; k--) {
+                if(curr_addr >= func_tracer_buf[k]->func_start_addr && curr_addr < func_tracer_buf[k]->func_end_addr) {
+                    fprintf(fp, " ret  [%s]\n", func_tracer_buf[k]->func_name);
+                    printf("ret  [%s] to [%s]\n", func_tracer_buf[k]->func_name, func_tracer_buf[i]->func_name);
                 }
             }
 
