0d3dc617f5c7b5c5e8e0922d6028436bffefb6cd
tracer-ics2022 <tracer@njuics.org>
2022-10-22 11:35:41 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:35:41 up  2:09,  1 user,  load average: 0.55, 0.26, 0.17

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -106,7 +106,7 @@ void *memmove(void *dst, const void *src, size_t n) {
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not tested");
+  //panic("Not tested");
   void *end_of_out = out;
   while(n--) {
     *(char *)end_of_out = *(char *)in;
