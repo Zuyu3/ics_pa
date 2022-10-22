f3e676c2c9ec99886b4a933dd9df913a04bc76ba
tracer-ics2022 <tracer@njuics.org>
2022-10-22 21:58:21 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:58:21 up  6:34,  1 user,  load average: 0.24, 0.29, 0.27

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,17 +18,19 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  printf("pc is: %x   %x\n", ref_r->pc, cpu.pc);
+  printf("pc is: %x   %x\n", ref_r->pc, pc);
   for(int i = 0; i < 32; i++) {
     printf("%d:  %x   %x\n", i, gpr(i), ref_r->gpr[i]);
   }
   printf("\n\n\n");
   getchar();
-  if(cpu.pc != ref_r->pc)
+  if(pc != ref_r->pc)
     return false;
   for(int i = 0; i < 32; i++) {
-    if(ref_r->gpr[i] != gpr(i))
+    if(ref_r->gpr[i] != gpr(i)){
+      printf("reg %d Error: DUT is %x, while REF is %x\n", i, gpr(i), ref_r->gpr[i]);
       return false;
+    }
   }
   return true;
 }
