91f7206212ff24db6ef244f087febe2546f389eb
tracer-ics2022 <tracer@njuics.org>
2022-10-23 05:53:33 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  05:53:33 up  2:27,  3 users,  load average: 0.35, 0.40, 0.30

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -6,10 +6,10 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  printf("%ld   ", uptime->us);
+  printf("%d %d  ", uptime->us, uptime->us >> 32);
   printf("%d   ",inl(RTC_ADDR));
   //ioe_read(uptime->us, (RTC_ADDR));
-  printf("%ld\n", uptime->us);
+  printf("%ld\n", uptime->us, uptime->us >> 32);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
