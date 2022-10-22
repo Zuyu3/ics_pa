e4cadf7f60eb887be1c69f72c024a28791287fe5
tracer-ics2022 <tracer@njuics.org>
2022-10-22 21:43:42 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:43:42 up  6:19,  1 user,  load average: 0.33, 0.31, 0.27

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,6 +18,12 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+  if(pc != ref_r->pc)
+    return false;
+  for(int i = 0; i < 32; i++) {
+    if(ref_r->gpr[i] != gpr(i))
+      return false;
+  }
   return true;
 }
 
