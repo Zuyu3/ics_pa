f7ed12c10b50337fe52edafe870bc5173d81873c
tracer-ics2022 <tracer@njuics.org>
2022-10-22 17:52:36 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:52:36 up  2:28,  1 user,  load average: 0.10, 0.31, 0.34

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -57,7 +57,7 @@ int sprintf(char *out, const char *fmt, ...) {
           case 's':
               strcpy(out + res, va_arg(ap, char*));
               res += strlen(out + res);
-              if(res != 7)
+              if(res != 6)
               panic("wrong index");
               break;
           case 'c':
