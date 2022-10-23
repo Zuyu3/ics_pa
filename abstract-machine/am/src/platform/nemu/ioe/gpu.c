6da531b8980f696730e760001093245cd67c685f
tracer-ics2022 <tracer@njuics.org>
2022-10-23 20:47:47 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:47:47 up  3:11,  1 user,  load average: 0.40, 0.28, 0.25

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -6,8 +6,9 @@
 
 void __am_gpu_init() {
   int i;
-  int w = inw(VGACTL_ADDR + 2);  // TODO: get the correct width
-  int h = inw(VGACTL_ADDR);  // TODO: get the correct height
+  int w = io_read(AM_GPU_CONFIG).width;  // TODO: get the correct width
+  int h = io_read(AM_GPU_CONFIG).height;  // TODO: get the correct height
+  printf("%d, %d", w, h);
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
@@ -16,7 +17,7 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = inw(VGACTL_ADDR), .height = (VGACTL_ADDR + 2),
     .vmemsz = 0
   };
 }
