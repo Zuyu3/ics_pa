91112a14b5f5a06ba1da5aadb11c5ca66f2f126d
tracer-ics2022 <tracer@njuics.org>
2022-10-23 11:13:02 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:13:02 up  3:05,  1 user,  load average: 0.21, 0.29, 0.28

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -6,7 +6,7 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   kbd->keycode = inl(KBD_ADDR);
-  printf("%d\n", inl(KBD_ADDR));
+  printf("%d %d\n", inl(KBD_ADDR), inl(KBD_ADDR + 4));
 
   kbd->keydown = kbd->keycode? true: false;
   if(kbd->keydown) {
