0d762159b7ed828e1153fa183367d0c6bde8a341
tracer-ics2022 <tracer@njuics.org>
2022-10-23 21:13:25 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:13:25 up  3:36,  1 user,  load average: 0.67, 0.32, 0.25

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -14,7 +14,7 @@ void __am_gpu_init() {
   int h = GPU_H;  // TODO: get the correct height
   printf("%d, %d", w, h);
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i ++) fb[i] = 256;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
 }
 
