6424632a6833bde284f84723d9ac37637765786f
tracer-ics2022 <tracer@njuics.org>
2022-10-24 18:09:53 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:09:53 up  3:19,  1 user,  load average: 0.20, 0.20, 0.34

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -24,7 +24,7 @@ char *strcpy(char *dst, const char *src) {
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not tested");
+  //panic("Not tested");
   char *end_of_dst = dst;
   while(n--) {
     if(*src == '\0')
@@ -66,7 +66,7 @@ int strcmp(const char *s1, const char *s2) {
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not tested");
+  //panic("Not tested");
   while(n--) {
     if(*s1 != *s2 || *s1 == '\0')
       return *s2 - *s2;
@@ -86,7 +86,7 @@ void *memset(void *s, int c, size_t n) {
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not tested");
+  //panic("Not tested");
   char src_cpy[n];
   void *end_of_dst = dst;
 
