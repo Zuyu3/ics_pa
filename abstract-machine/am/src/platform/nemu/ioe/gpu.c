70e4cf4591a5546c8736082f17fe351cea508e35
tracer-ics2022 <tracer@njuics.org>
2022-10-24 09:52:17 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:52:17 up 2 min,  1 user,  load average: 0.74, 0.59, 0.25

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -9,14 +9,14 @@
 
 
 void __am_gpu_init() {
-  /*
+  
   int i;
   int w = GPU_W;  // TODO: get the correct width
   int h = GPU_H;  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) {fb[i] = i * 256 / w * h; fb[i] |= fb[i] << 16 | fb[i] << 8;}
   outl(SYNC_ADDR, 1);
-  */
+  
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -30,10 +30,11 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  uint32_t *sr = (uint32_t *)(uintptr_t)ctl->pixels;
+  //uint32_t *sr = (uint32_t *)(uintptr_t)ctl->pixels;
   for(int i = 0; i < ctl->h; i++)
     for(int j = 0; j< ctl->w; j++)
-       fb[(ctl->y + i) * GPU_W + ctl->x + j] = sr[i * ctl->w + j];
+       //fb[(ctl->y + i) * GPU_W + ctl->x + j] = sr[i * ctl->w + j];
+       fb[(ctl->y + i) * GPU_W + ctl->x + j] = 0;
   
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
