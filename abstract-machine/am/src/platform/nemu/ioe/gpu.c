bc3e4ec71d03a98a099ac836296c20e27166b47a
tracer-ics2022 <tracer@njuics.org>
2022-10-24 10:12:54 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:12:54 up 22 min,  1 user,  load average: 0.64, 0.53, 0.38

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -30,11 +30,10 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  //uint32_t *sr = (uint32_t *)(uintptr_t)ctl->pixels;
+  uint32_t *sr = (uint32_t *)(uintptr_t)ctl->pixels;
   for(int i = 0; i < ctl->h; i++)
     for(int j = 0; j< ctl->w; j++)
-       //fb[(ctl->y + i) * GPU_W + ctl->x + j] = sr[i * ctl->w + j];
-       fb[(ctl->y + i) * GPU_W + ctl->x + j] = 0;
+       fb[(ctl->y + i) * GPU_W + ctl->x + j] = sr[i * ctl->w + j];
   
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
