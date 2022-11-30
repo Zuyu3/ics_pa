a948f448710159bcbd936007acc25330614582fd
tracer-ics2022 <tracer@njuics.org>
2022-11-30 23:22:33 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:22:33 up 47 min,  1 user,  load average: 0.25, 0.17, 0.18

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -22,6 +22,7 @@ extern void __am_asm_trap(void);
 
 bool cte_init(Context*(*handler)(Event, Context*)) {
   // initialize exception entry
+  //mtvec = 0x80528
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
 
   // register event handler
