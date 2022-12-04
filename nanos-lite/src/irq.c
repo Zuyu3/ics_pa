f9e5fcdd0e8422c4fb2510936f8e51a42ad821ca
tracer-ics2022 <tracer@njuics.org>
2022-12-04 21:16:02 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:16:02 up  3:18,  1 user,  load average: 0.47, 0.28, 0.16

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,6 +6,7 @@ static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD:
       printf("Successfully triggered YIELD.\n");
+      break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
