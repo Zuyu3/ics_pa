3d9a6bcb9c0cd874819837a6080320cb72544a8a
tracer-ics2022 <tracer@njuics.org>
2022-12-06 10:14:58 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:14:58 up 6 min,  1 user,  load average: 0.19, 0.32, 0.18

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,9 +16,18 @@ size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr elf_header;
+  Elf_Phdr elf_prog_header;
   ramdisk_read(&elf_header, 0, sizeof(elf_header));
-    printf("%x %x %x %x %x\n", elf_header.e_machine, elf_header.e_entry, elf_header.e_phoff, elf_header.e_phentsize, elf_header.e_phnum);
+  assert(*(uint32_t *)elf_header.e_ident == 0x7f454c46);
+
+  for(int i = 0; i < elf_header.e_phnum; i++) {
+    ramdisk_read(&elf_prog_header, elf_header.e_phoff + elf_header.e_phentsize * i, elf_header.e_phentsize);
+    printf("%u  %x  %x  %x  %u  %u\n\n", elf_prog_header.p_type, elf_prog_header.p_offset, elf_prog_header.p_vaddr, elf_prog_header.p_paddr, elf_prog_header.p_filesz, elf_prog_header.p_memsz);
+
+  }  
+
   return 0;
+
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
