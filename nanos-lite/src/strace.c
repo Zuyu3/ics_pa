c0cc831049a45b28522e0d2337c6413435210f34
tracer-ics2022 <tracer@njuics.org>
2022-12-06 20:11:24 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:11:24 up  1:49,  1 user,  load average: 0.64, 0.61, 0.56

---
+++ nanos-lite/src/strace.c
@@ -0,0 +1,44 @@
+#include <common.h>
+
+#define STRACE_SIZE 100
+
+const char call_name[][20] = {"exit", "yield"};
+typedef struct 
+{
+    uintptr_t a[4];
+    uintptr_t res;
+} strace_log;
+
+static strace_log strace_buf[STRACE_SIZE];
+static int strace_index = 0;
+
+void add_strace_log(uintptr_t *ar, uintptr_t r) {
+    #ifndef CONFIG_STRACE
+      return;
+    #endif
+
+    strace_buf[strace_index].a[0] = ar[0];
+    strace_buf[strace_index].a[1] = ar[1];
+    strace_buf[strace_index].a[2] = ar[2];
+    strace_buf[strace_index].a[3] = ar[3];
+    strace_buf[strace_index].res = r;
+    strace_index = (strace_index + 1) % STRACE_SIZE;
+}
+
+void print_sbuf_log() {
+    #ifndef CONFIG_STRACE
+      return;
+    #endif
+
+
+  printf("Here are the %d most recent SYSTEM CALLs.\n\n", STRACE_SIZE);
+  int id = strace_index;
+  do {
+    if(strace_buf[id].a[0] != 0)
+      printf("SYS_%-8s (%-4d, %-4d, %-4d, %-4d) : return %-4d\n");
+
+    id = (id + 1) % STRACE_SIZE;
+  } while(id != strace_index);
+
+  printf("\n\n");
+}
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,12 +1,19 @@
 #include <common.h>
 #include "syscall.h"
 
+void add_strace_log(uintptr_t *ar, uintptr_t r);
+void print_sbuf_log();
+
 uintptr_t sys_yield() {
   yield();
   return 0;
 }
 
 uintptr_t sys_exit() {
+  #ifdef CONFIG_STRACE
+    print_sbuf_log();
+  #endif
+
   halt(0);
   return 0;
 }
@@ -16,12 +23,11 @@ void do_syscall(Context *c) {
   uintptr_t a[4];
   
   a[0] = c->GPR1;
-  //printf("syscall arg 0 is: %d\n", a[0]);
-  /*
   a[1] = c->GPR2;
   a[2] = c->GPR3;
-  a[3] = c-> GPR4
-  */
+  a[3] = c->GPR4;
+  //printf("syscall arg 0 is: %d\n", a[0]);
+
 
   switch (a[0]) {
     case SYS_yield:
@@ -33,4 +39,8 @@ void do_syscall(Context *c) {
       
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+
+  #ifdef CONFIG_STRACE
+    add_strace_log(a, c->GPRx);
+  #endif
 }
