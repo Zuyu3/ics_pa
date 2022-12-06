b6685d62ff2375ab5ac3f3fbeebedaadacd60c84
tracer-ics2022 <tracer@njuics.org>
2022-12-06 10:17:36 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:17:36 up 9 min,  1 user,  load average: 0.37, 0.37, 0.22

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -47,7 +47,7 @@ int format_helper_uint2str(char *out, unsigned int d) {
 
 int format_helper_uint20xstr(char *out, unsigned d) {
     //strcpy(out, "0x");
-    return format_helper_value2str(out + 2, d, 16) + 2;
+    return format_helper_value2str(out, d, 16);
 } 
 
 int printf(const char *fmt, ...) {
