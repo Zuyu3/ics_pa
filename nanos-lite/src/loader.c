9d46358baa10126b509b76eaa842a8d9001bf09e
tracer-ics2022 <tracer@njuics.org>
2022-12-06 11:53:39 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:53:39 up  1:45,  1 user,  load average: 0.25, 0.33, 0.30

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,16 +10,29 @@
 #endif
 
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
-size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+//size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 
+
+#if defined(__ISA_AM_NATIVE__)
+# define EXPECT_TYPE EM_X86_64
+#elif defined(__ISA_RISCV32__)
+# define EXPECT_TYPE EM_RISCV
+#else
+# error Unsupported ISA
+#endif
 
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
+  #ifdef __ISA_
+  printf("%d", 1)
+  #endif
   Elf_Ehdr elf_header;
   Elf_Phdr prog_header;
   ramdisk_read(&elf_header, 0, sizeof(elf_header));
 
   assert(*(uint32_t *)elf_header.e_ident == 0x464c457f);
+  printf("%d  %d\n", elf_header.e_machine, EXPECT_TYPE);
+  assert(elf_header.e_machine == EXPECT_TYPE);
 
   for(int i = 0; i < elf_header.e_phnum; i++) {
     ramdisk_read(&prog_header, elf_header.e_phoff + elf_header.e_phentsize * i, elf_header.e_phentsize);
