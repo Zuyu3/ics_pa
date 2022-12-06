020fd9b422e7387957d5afdf180cc2957721e266
tracer-ics2022 <tracer@njuics.org>
2022-12-06 10:25:11 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:25:11 up 16 min,  1 user,  load average: 0.22, 0.27, 0.23

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,15 +16,16 @@ size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr elf_header;
-  Elf_Phdr elf_prog_header;
+  Elf_Phdr prog_header;
   ramdisk_read(&elf_header, 0, sizeof(elf_header));
   printf("magic number: %x\n", *(uint32_t *)elf_header.e_ident);
   assert(*(uint32_t *)elf_header.e_ident == 0x464c457f);
 
   for(int i = 0; i < elf_header.e_phnum; i++) {
-    ramdisk_read(&elf_prog_header, elf_header.e_phoff + elf_header.e_phentsize * i, elf_header.e_phentsize);
-    printf("%u  %x  %x  %x  %x  %x\n\n", elf_prog_header.p_type, elf_prog_header.p_offset, elf_prog_header.p_vaddr, elf_prog_header.p_paddr, elf_prog_header.p_filesz, elf_prog_header.p_memsz);
-
+    ramdisk_read(&prog_header, elf_header.e_phoff + elf_header.e_phentsize * i, elf_header.e_phentsize);
+    if(prog_header.p_type != PT_LOAD)
+      continue;
+    printf("flag is: %x\n", prog_header.p_flags);
   }  
 
   return 0;
