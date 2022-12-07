fa3dd9c2965534b27225cf111543f22fe69b2240
tracer-ics2022 <tracer@njuics.org>
2022-12-07 21:28:15 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:28:15 up  2:52,  1 user,  load average: 0.26, 0.43, 0.37

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -42,7 +42,7 @@ void init_fs() {
 int fs_open(const char *pathname, int flags, int mode) {
   //printf("%d\n", sizeof(file_table) / sizeof(Finfo));
   for(int i = 0; i < sizeof(file_table) / sizeof(Finfo); i++) {
-    if(strcmp(pathname, file_table[i].name)){
+    if(strcmp(pathname, file_table[i].name) == 0){
       file_table[i].open_offset = 0;
       return i;
     }
