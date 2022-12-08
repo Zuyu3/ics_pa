7be830179e2ba87421a118fc47c706957d5fccd6
tracer-ics2022 <tracer@njuics.org>
2022-12-08 18:22:26 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:22:26 up  2:20,  1 user,  load average: 0.95, 0.83, 0.64

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -14,11 +14,12 @@ int main() {
 
   while(1) {
     gettimeofday(&current_time, NULL);
-    //printf("current: %ld  %ld\n", current_time.tv_sec, current_time.tv_usec);
 
     if((current_time.tv_usec - last_time.tv_usec) > 500000) {
       last_time.tv_usec = current_time.tv_usec;
       last_time.tv_sec = current_time.tv_sec;
+      printf("last: %ld  %ld\n", last_time.tv_sec, last_time.tv_usec);
+
       count++;
       printf("time output %d.\n", count);
       if(count == 40) {
