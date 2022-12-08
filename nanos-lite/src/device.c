a201b794f5726f3e87058708ad122e4206bb555b
tracer-ics2022 <tracer@njuics.org>
2022-12-09 01:04:37 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:04:37 up  1:50,  1 user,  load average: 0.52, 0.81, 0.82

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -66,7 +66,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   //int width, height;
   x = (offset / 4) % screen_w;
   y = (offset / 4) / screen_w;
-  printf("os write: (%d, %d), len=%d\n\n", x, y, offset, len);
+  printf("os write: (%d, %d), len=%d\n\n", x, y, len);
   
 
   //TODO:Maybe bugs here.
