60a0e1c6e25a884a79ad6a26e5485f6e6c31b143
tracer-ics2022 <tracer@njuics.org>
2022-12-08 19:08:31 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:08:31 up  2:43,  1 user,  load average: 0.44, 0.36, 0.35

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,8 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
-#include <sys/time.h>
-
-struct timeval last_time, current_time;
+#include <NDL.h>
 
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
@@ -10,15 +8,15 @@ int main() {
 
   int count = 0;
 
-  gettimeofday(&last_time, NULL);
+  uint32_t last_time, current_time;
+  last_time = NDL_GetTicks();
 
   while(1) {
-    gettimeofday(&current_time, NULL);
+    current_time = NDL_GetTicks();
 
-    if(current_time.tv_usec - last_time.tv_usec + (current_time.tv_sec - last_time.tv_sec) * 1000000> 500000) {
-      last_time.tv_usec = current_time.tv_usec;
-      last_time.tv_sec = current_time.tv_sec;
-      //printf("last: %ld  %ld\n", last_time.tv_sec, last_time.tv_usec);
+    if(current_time - last_time > 5000) {
+      last_time = current_time;
+      printf("last: %u  %u\n", last_time, current_time);
 
       count++;
       printf("time output %d.\n", count);
