c4f4f7e5403804b43d4a8603a9fc6dbdcdbd00dd
tracer-ics2022 <tracer@njuics.org>
2022-12-08 21:55:44 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:55:44 up  5:30,  1 user,  load average: 0.57, 0.51, 0.45

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -80,7 +80,8 @@ int NDL_Init(uint32_t flags) {
 
   char buf[64];
   FILE *fp = fopen("/proc/dispinfo", "r");
-  int config_len = fread(buf, 64, 1, fp);
+  int config_len =  fscanf(fp, "%s", buf);
+  //int config_len = fread(buf, 64, 1, fp);
   printf("%d   %d\n", config_len, strlen(buf));
   fclose(fp);
   printf("gpu config is: %s\n\n\n", buf);
