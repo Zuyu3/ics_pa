a0ad816c193929e4df0dc2d229b0ee9d697ec63d
tracer-ics2022 <tracer@njuics.org>
2022-12-08 18:08:00 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:08:00 up  2:05,  1 user,  load average: 0.63, 0.57, 0.53

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,16 +1,8 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <sys/time.h>
 
-typedef struct
-{
-    uint64_t tv_sec;     /* seconds */
-    uint64_t tv_usec;    /* microseconds */
-} timeval;
-
-int gettimeofday(timeval *tv, timeval *tz);
-
-timeval last_time;
-timeval current_time;
+struct timeval last_time, current_time;
 
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
@@ -21,11 +13,10 @@ int main() {
   printf("%p  %p\n", &last_time, &current_time);
   
   gettimeofday(&last_time, NULL);
-  printf("%lld  %lld\n", last_time.tv_sec, last_time.tv_usec);
 
   while(1) {
     gettimeofday(&current_time, NULL);
-    printf("current: %lld  last: %lld\n", current_time.tv_sec, last_time.tv_sec);
+    printf("current: %ld  last: %ld\n", current_time.tv_sec, last_time.tv_sec);
 
     if((current_time.tv_sec - last_time.tv_sec) > 0) {
       last_time.tv_usec = current_time.tv_usec;
