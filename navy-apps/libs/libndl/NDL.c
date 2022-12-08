b762258c1db5ffd4d26252f93b8bb3ac902bb98e
tracer-ics2022 <tracer@njuics.org>
2022-12-08 23:16:47 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:16:47 up 3 min,  1 user,  load average: 0.44, 0.40, 0.18

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -89,7 +89,7 @@ int NDL_Init(uint32_t flags) {
 
   int id = 0, temp, temp_w = -1, temp_h = -1;
   while(id < 64) {
-    printf("id:%d\n", id);
+    printf("id:%d  %c\n", id, buf[id]);
       if(strcmp(buf + id, "WIDTH") == 0) {
         printf("hit width at %d\n", id);
         temp_w = 0;
