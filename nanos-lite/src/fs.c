652eec1c5f81c30de1a92c5cebb83eb4143291ec
tracer-ics2022 <tracer@njuics.org>
2022-12-09 00:22:25 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:22:25 up  1:08,  1 user,  load average: 0.36, 0.38, 0.36

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -8,6 +8,7 @@ size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 size_t serial_write(const void *buf, size_t offset, size_t len);
 size_t events_read(void *buf, size_t offset, size_t len);
 size_t dispinfo_read(void *buf, size_t offset, size_t len);
+size_t fb_write(const void *buf, size_t offset, size_t len);
 
 typedef struct {
   char *name;
@@ -35,9 +36,9 @@ static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
   [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
   [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
+  [FD_FB] = {"/dev/fb", 0, 0, invalid_read, invalid_write},
   {"/dev/events", 0, 0, events_read, invalid_write},
-  {"/dev/fb", 0, 0, invalid_read, invalid_write},
-  [FD_FB] = {"/proc/dispinfo", 0, 0, dispinfo_read, invalid_write},
+  {"/proc/dispinfo", 0, 0, dispinfo_read, invalid_write},
 #include "files.h"
 };
 
