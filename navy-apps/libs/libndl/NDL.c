a662e9bee7baa43fbdb71c43102e7587c55e6bb3
tracer-ics2022 <tracer@njuics.org>
2022-12-08 22:16:20 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:16:20 up  5:51,  1 user,  load average: 1.18, 0.82, 0.56

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -83,6 +83,7 @@ int NDL_Init(uint32_t flags) {
   char tokens[32];
   FILE *fp = fopen("/proc/dispinfo", "r");
   fscanf(fp, "%s", buf);
+  printf("%s\n", buf);
   int config_len =  strlen(buf);
   printf("%d\n", config_len);
 
