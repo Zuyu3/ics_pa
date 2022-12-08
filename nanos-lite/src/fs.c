207049ba8484c870fcf4eedcd1c39981274aac99
tracer-ics2022 <tracer@njuics.org>
2022-12-09 00:43:49 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:43:49 up  1:30,  1 user,  load average: 0.52, 0.51, 0.44

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -72,7 +72,7 @@ size_t fs_write(int fd, const void *buf, size_t len) {
   assert(fd >=0 && fd < sizeof(file_table) / sizeof(Finfo));
 
   if(file_table[fd].write != NULL) {
-    len = file_table[fd].write(buf, 0, len);
+    len = file_table[fd].write(buf, file_table[fd].open_offset, len);
   }
   else {
     len = (len <= file_table[fd].size - file_table[fd].open_offset) ? len: file_table[fd].size - file_table[fd].open_offset;
