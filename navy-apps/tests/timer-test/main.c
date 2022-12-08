63ac3407696cce92884e7e4195e5ec479df4698d
tracer-ics2022 <tracer@njuics.org>
2022-12-08 18:01:03 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:01:03 up  1:58,  1 user,  load average: 0.61, 0.46, 0.47

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,7 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
 
-
 typedef struct
 {
     uint64_t tv_sec;     /* seconds */
@@ -10,26 +9,23 @@ typedef struct
 
 int gettimeofday(struct timeval *tv, struct timeval *tz);
 
+struct timeval last_time;
+struct timeval current_time;
 
-#ifdef __ISA_NATIVE__
-#error can not support ISA=native
-#endif
-
-#define SYS_yield 1
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
-  struct timeval last_time;
-  struct timeval current_time;
+
   int count = 0;
+
+  printf("%p  %p\n", &last_time, &current_time);
   
   gettimeofday(&last_time, NULL);
   printf("%ld  %ld\n", last_time.tv_sec, last_time.tv_usec);
 
   while(1) {
     gettimeofday(&current_time, NULL);
-    printf("%ld  %ld\n", last_time.tv_sec, last_time.tv_usec);
-    printf("%ld  %ld\n\n\n", current_time.tv_sec, current_time.tv_usec);
+    printf("current time: %ld  %ld\n\n\n", current_time.tv_sec, current_time.tv_usec);
 
     if((current_time.tv_usec - last_time.tv_usec) > 500000) {
       last_time.tv_usec = current_time.tv_usec;
@@ -42,5 +38,5 @@ int main() {
     }
   }
   
-  return _syscall_(SYS_yield, 0, 0, 0);
+   return 0;
 }
