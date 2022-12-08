37dc29742d4d8b7d280562d7dcee115e49ea21e7
tracer-ics2022 <tracer@njuics.org>
2022-12-08 21:42:35 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:42:35 up  5:17,  1 user,  load average: 0.39, 0.42, 0.32

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -47,7 +47,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   AM_GPU_CONFIG_T gpu_info = io_read(AM_GPU_CONFIG);
   int res;
-  printf("write:\nWIDTH:%d\nHEIGHT:%d\n\n");
+  printf("write:\nWIDTH:%d\nHEIGHT:%d\n\n", gpu_info.width, gpu_info.height);
   res = sprintf(buf, "WIDTH:%d\nHEIGHT:%d", gpu_info.width, gpu_info.height);
   if(res > len)
     panic("Error. Buf too small to read dispinfo\n");
