df2c37b69ad41eafc12a68f4224ebdc864b15a57
tracer-ics2022 <tracer@njuics.org>
2022-12-10 19:34:26 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:34:26 up 41 min,  1 user,  load average: 0.09, 0.10, 0.05

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -9,6 +9,7 @@
 static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
+static int canvas_x = 0, canvas_y = 0;
 int open(const char *path, int flags, mode_t mode);
 
 uint32_t NDL_GetTicks() {
@@ -61,7 +62,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   int fd = open("/dev/fb", 0, 0);
 
   for(int i = 0; i < h; i++) {
-    lseek(fd, ((y + i) * screen_w + x) * 4, SEEK_SET);
+    lseek(fd, ((canvas_y + y + i) * screen_w + x + canvas_x) * 4, SEEK_SET);
     write(fd, pixels + w * i, w * 4);
   }
   close(fd);
