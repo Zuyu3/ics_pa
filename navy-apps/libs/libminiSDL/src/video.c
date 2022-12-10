023f9023fe6ded387774cc2a4c0fcb69100b052f
tracer-ics2022 <tracer@njuics.org>
2022-12-10 23:07:23 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:07:23 up 14 min,  1 user,  load average: 0.25, 0.32, 0.26

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -21,7 +21,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
   dst_y = dstrect == NULL ? 0 : dstrect->y;
   src_x = srcrect == NULL ? 0 : srcrect->x;
   src_y = srcrect == NULL ? 0 : srcrect->y;
-  printf("\n(copy_w, copy_h, dst_x, dst_y, dst->w, dst->h, src_x, src_y, src->w, src->h): %u %u %d %d %d %d %d %d %d %d\n\n", copy_w, copy_h, dst_x, dst_y, dst->w, dst->h, src_x, src_y, src->w, src->h);
+  printf("\n(copy_w, copy_h, dst_x, dst_y, dst->w, dst->h, src_x, src_y, src->w, src->h)\n %u %u %d %d %d %d %d %d %d %d\n\n", copy_w, copy_h, dst_x, dst_y, dst->w, dst->h, src_x, src_y, src->w, src->h);
   printf("%p  %p\n", src->pixels, dst->pixels);
 
   for(int j = 0; j < copy_h; j++) {
