ca0f0bb27458ed94f953c935c1dcf96038a576fa
tracer-ics2022 <tracer@njuics.org>
2022-12-10 23:31:48 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:31:48 up 39 min,  1 user,  load average: 0.51, 0.57, 0.41

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -6,12 +6,9 @@
 #include <stdio.h>
 
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
-  printf("Call SDL BlitSurface\n");
   assert(dst && src);
   assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
-  //uint8_t *sp = src->pixels, *dp = dst->pixels;
   uint32_t *sp = (uint32_t *)src->pixels, *dp = (uint32_t *)dst->pixels;
-  //printf("%p %p %p %p\n", dst, src, dstrect, srcrect);
 
   uint16_t copy_w, copy_h;
   int16_t dst_x, dst_y, src_x, src_y;
@@ -21,28 +18,19 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
   dst_y = dstrect == NULL ? 0 : dstrect->y;
   src_x = srcrect == NULL ? 0 : srcrect->x;
   src_y = srcrect == NULL ? 0 : srcrect->y;
-  /*
-  printf("copy_w:%d, copy_h:%d\n", copy_w, copy_h);
-  printf("dst_x:%d, dst_y:%d, dst->w:%d, dst->h:%d\n", dst_x, dst_y, dst->w, dst->h);
-  printf("src_x:%d, src_y:%d, src->w:%d, src->h:%d\n", src_x, src_y, src->w, src->h);
-  */
 
   for(int j = 0; j < copy_h; j++) {
-    //printf("bias = %d\n", (dst_y + j) * dst->w);
     for(int i = 0; i < copy_w; i++) {
       dp[(dst_y + j) * dst->w + dst_x + i] = sp[(src_y + j) * src->w + src_x + i];
     }
   }
 
-  printf("Leave SDL BlitSurface\n");
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
-  printf("Call SDL FillRect\n");
 
   uint32_t *p = (uint32_t *) dst->pixels;
   int bias = 0;
-  printf("h: %d  w: %d\n", dst->h, dst->w);
   if(!dstrect) {
     for(int i = 0; i < dst->h * dst->w; i++) {
       p[i] = color;
@@ -56,7 +44,6 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
       }
     }
   }
-  printf("Leave SDL FillRect\n");
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
