fdda421f8b1c2c14bb0290e00d9507f9ca976959
tracer-ics2022 <tracer@njuics.org>
2022-12-11 12:11:53 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:11:53 up  2:30,  1 user,  load average: 0.46, 0.57, 0.74

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -67,6 +67,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
     printf("call SDL UpdateRect 8 bits\n");
     SDL_Color *colours = s->format->palette->colors;
     for(int j = 0; j < rect_h; j++) {
+      printf("line %d: colours is %-8x\n", j, colours[s->pixels[(y + j) * s->w + x]].val);
       for(int i = 0; i < rect_w; i++) {
         //printf("(%d, %d): id= %u, colours: %-8x\n", j, i, s->pixels[(y + j) * s->w + x + i], colours[s->pixels[(y + j) * s->w + x + i]].val);
         new_pixels[j * rect_w + i] = colours[s->pixels[(y + j) * s->w + x + i]].val;
