d01e524afa946ee4ea853cafa878bb512a08268f
tracer-ics2022 <tracer@njuics.org>
2022-12-11 11:29:58 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:29:58 up  1:48,  1 user,  load average: 0.37, 0.54, 0.43

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -58,7 +58,7 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
   printf("call SDL UpdateRect\n");
   int rect_w = w, rect_h = h;
-  if(w == 0 && y == 0 && w == 0 && h == 0) {
+  if(x == 0 && y == 0 && w == 0 && h == 0) {
     rect_w = s->w;
     rect_h = s->h;
   }
