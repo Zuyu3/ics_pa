265bd430fbe97b7fa319d052fa96e13f862c4b13
tracer-ics2022 <tracer@njuics.org>
2022-12-11 11:54:15 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:54:15 up  2:12,  1 user,  load average: 0.65, 1.02, 0.81

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -62,12 +62,13 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
     rect_w = s->w;
     rect_h = s->h;
   }
+  
   uint32_t *new_pixels = (uint32_t *)malloc(rect_w * rect_h * 4);
   if(s->format->BitsPerPixel == 8) {
     SDL_Color *colours = s->format->palette->colors;
     for(int j = 0; j < rect_h; j++) {
       for(int i = 0; i < rect_w; i++) {
-        printf("(%d, %d): id= %u, colours: %-8x\n", i, j, s->pixels[(y + j) * s->w + x + i], colours[s->pixels[(y + j) * s->w + x + i]].val);
+        printf("(%d, %d): id= %u, colours: %-8x\n", j, i, s->pixels[(y + j) * s->w + x + i], colours[s->pixels[(y + j) * s->w + x + i]].val);
         new_pixels[j * rect_w + i] = s->format->palette->colors[s->pixels[(y + j) * s->w + x + i]].val;
       }
     }
