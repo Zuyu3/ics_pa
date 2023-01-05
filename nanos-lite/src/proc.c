e2d5bd650d336a986cd6232529e4299122cc5a42
tracer-ics2022 <tracer@njuics.org>
2023-01-05 23:17:40 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:17:40 up 10:31,  1 user,  load average: 0.87, 0.50, 0.39

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -30,7 +30,7 @@ void init_proc() {
   context_kload(&pcb[0], hello_fun, (void *)0);
 
 
-  context_uload(&pcb[1], "/bin/pal", test_arg, test_env);
+  context_uload(&pcb[1], "/bin/pal", test_arg, NULL);
 
   switch_boot_pcb();
 
--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -6,7 +6,6 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  printf("call main get argu: %p\n", args);
   int argc = *(int *)args;
   char **argv, **envp;
   argv = *((void **)args + 1);
