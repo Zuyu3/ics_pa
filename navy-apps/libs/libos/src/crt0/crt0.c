587a42978023328e1507129a793ffe62fb18620d
tracer-ics2022 <tracer@njuics.org>
2023-01-05 23:06:25 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:06:25 up 10:19,  1 user,  load average: 0.21, 0.30, 0.32

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -1,12 +1,16 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  char *empty[] =  {NULL };
-  environ = empty;
-  exit(main(0, empty, empty));
+  int argc = *(int *)args;
+  char *argv[] = *((char **)args + 1);
+  char *envp[] = *((char **)args + 1);
+  printf("%d  %p  %p\n", argc, argv, envp);
+  environ = envp;
+  exit(main(argc, argv, envp));
   assert(0);
 }
