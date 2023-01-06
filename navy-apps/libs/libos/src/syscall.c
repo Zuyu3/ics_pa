05f3460704d819c73200c93b2e460dfa158f59eb
tracer-ics2022 <tracer@njuics.org>
2023-01-06 15:11:21 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:11:21 up  2:35,  1 user,  load average: 1.32, 0.65, 0.52

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -107,9 +107,7 @@ int _gettimeofday(struct timeval *tv, struct timezone *tz) {
 }
 
 int _execve(const char *fname, char * const argv[], char *const envp[]) {
-  printf("libos call exec with filename %p\n", fname);
   return _syscall_(SYS_execve, (intptr_t)fname, (intptr_t)argv, (intptr_t)envp);
-  return 0;
 }
 
 // Syscalls below are not used in Nanos-lite.
