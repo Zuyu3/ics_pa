c817cebdd5473283ca8b2c945f81061c6abb98c9
tracer-ics2022 <tracer@njuics.org>
2023-01-06 18:52:53 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:52:53 up  6:08,  1 user,  load average: 0.32, 0.24, 0.11

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -84,6 +84,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
     while(argv[argc])
       argc++;
   }
+  printf("-----------\n");
   if(envp) {
     while(envp[envc])
       envc++;
