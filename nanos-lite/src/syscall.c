79b1458b5e2788efcaf7f7b373a320f4a1a7a595
tracer-ics2022 <tracer@njuics.org>
2023-01-06 22:09:54 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:09:54 up  9:25,  1 user,  load average: 0.71, 0.53, 0.37

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -14,6 +14,7 @@ uintptr_t sys_exit(int t) {
     print_sbuf_log();
   #endif
 
+  printf("call sys_exit\n");
   execve_load_ucontext("/bin/nterm", NULL, NULL);
   //naive_uload(NULL, "/bin/nterm");
   return 0;
