6c5442bfc432eec81ccde9ded659b8bf9a70674b
tracer-ics2022 <tracer@njuics.org>
2023-01-06 18:53:19 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:53:19 up  6:08,  1 user,  load average: 0.64, 0.34, 0.15

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -88,7 +88,6 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
     while(envp[envc])
       envc++;
   }
-  printf("-----------\n");
 
   char *arg_pointer[argc], *env_pointer[envc];
 
@@ -98,6 +97,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
     strcpy(stack_start, envp[i]);
     //printf("env[%d] at %p: %s, len=%d\n", i, env_pointer[i], env_pointer[i], strlen(envp[i]) + 1);
   }
+  printf("-----------\n");
 
   for(int i = argc - 1; i >= 0; i--) {
     stack_start -= (strlen(argv[i]) + 1);
