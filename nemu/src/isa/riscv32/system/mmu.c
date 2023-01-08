e432deb234fcc2fa4ce031c2b3307b3f93f8948b
tracer-ics2022 <tracer@njuics.org>
2023-01-08 23:55:37 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:55:37 up  2:21,  1 user,  load average: 0.36, 0.41, 0.33

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -21,9 +21,13 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   paddr_t vpn1 = vaddr >> 22, vpn0 = (vaddr >> 12) & 0x3ff, offset = vaddr & 0xfff;
   paddr_t page_table1_addr = (csr.satp << 12) + vpn1 * 4;
   
+  if((paddr_read(page_table1_addr, sizeof(paddr_t)) & 1) == 0)
+    printf("vaddr unmap error: %x\n", page_table1_addr);
   assert(paddr_read(page_table1_addr, sizeof(paddr_t)) & 1);
   paddr_t page_table0_addr = (paddr_read(page_table1_addr, sizeof(paddr_t)) >> 10 << 12) + vpn0 * 4;
   
+  if((paddr_read(page_table0_addr, sizeof(paddr_t)) & 1) == 0)
+    printf("vaddr unmap error: %x\n", page_table0_addr);
   assert(paddr_read(page_table0_addr, sizeof(paddr_t)) & 1);
   paddr_t paddr = (paddr_read(page_table0_addr, sizeof(paddr_t)) >> 10 << 12) + offset;
 
