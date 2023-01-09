5890e862289b585231e3a30896ac4e2e6b765a20
tracer-ics2022 <tracer@njuics.org>
2023-01-09 13:58:56 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:58:56 up  1:36,  1 user,  load average: 0.60, 0.29, 0.27

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -49,6 +49,7 @@ uintptr_t sys_lseek(int fd, size_t offset, int whence) {
 }
 
 uintptr_t sys_brk(void *new_brk) {
+  printf("call sys_brk: %p\n", new_brk);
   return mm_brk((uintptr_t)new_brk);
 }
 
