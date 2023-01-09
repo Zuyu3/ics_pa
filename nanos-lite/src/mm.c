a9edd589bc53c04c6ffecd4fdb3a4d17c0ce60bb
tracer-ics2022 <tracer@njuics.org>
2023-01-09 14:33:40 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:33:40 up  2:11,  1 user,  load average: 0.12, 0.23, 0.26

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -26,7 +26,7 @@ void free_page(void *p) {
 /* The brk() system call handler. */
 int mm_brk(uintptr_t brk) {
   uintptr_t max_brk = current->max_brk;
-  int page_num = (brk >> 12) - (max_brk >> 12);
+  int page_num = (brk >> 12) - ((max_brk - 1) >> 12);
   printf("brk from %p to %p, alloc %d pages\n", max_brk, brk, page_num);
   if(brk < max_brk)
     return 0;
