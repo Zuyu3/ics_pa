c54d207bd0da1122252c6ccae2203a91d7fc7114
tracer-ics2022 <tracer@njuics.org>
2023-01-09 21:33:02 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:33:02 up  9:10,  1 user,  load average: 1.33, 0.57, 0.44

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -23,6 +23,10 @@ Context* __am_irq_handle(Context *c) {
         }
         c->mepc += 4;
         break;
+      case 0x80000007:
+        printf("am switch to EVENT_IEQ_TIMER\n");
+        ev.event = EVENT_IRQ_TIMER;
+        break;
       default: ev.event = EVENT_ERROR; break;
     }
 
