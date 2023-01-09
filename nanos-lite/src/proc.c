a0604669b5ca0f3fcb26f4b8d2e0f4b8830f4b3e
tracer-ics2022 <tracer@njuics.org>
2023-01-09 14:23:22 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:23:22 up  2:01,  1 user,  load average: 0.40, 0.46, 0.33

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -48,8 +48,8 @@ void init_proc() {
   
   //context_kload(&pcb[0], hello_fun, (void *)0);
 
-  context_uload(new_pcb(), "/bin/pal", test_arg, NULL);
-  //context_uload(new_pcb(), "/bin/dummy", NULL, NULL);
+  //context_uload(new_pcb(), "/bin/pal", test_arg, NULL);
+  context_uload(new_pcb(), "/bin/dummy", NULL, NULL);
   //context_uload(new_pcb(), "/bin/nterm", NULL, NULL);
 
   switch_boot_pcb();
