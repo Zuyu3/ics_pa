101143da49afdb944445d533f2aaf47cad63706e
tracer-ics2022 <tracer@njuics.org>
2023-01-09 14:38:29 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:38:29 up  2:16,  1 user,  load average: 0.24, 0.23, 0.25

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -34,6 +34,7 @@ int mm_brk(uintptr_t brk) {
   assert(page_num > 0);
   void *page_alloced = new_page(page_num);
   for(int i = 0; i < page_num; i++) {
+    printf("map vaddr %p to paddr %p\n", (void *)((max_brk & ~0xfff) + 4096 * (i + 1)), (void *)(page_alloced + 4096 * i));
     map(&current->as, (void *)((max_brk & ~0xfff) + 4096 * (i + 1)), (void *)(page_alloced + 4096 * i), MMAP_READ | MMAP_WRITE);
   }
   current->max_brk = (brk & (~0xfff)) + 4096;
