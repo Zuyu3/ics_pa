3e1f155cd079ee5dff326e2f4c4d7759635f1e5f
tracer-ics2022 <tracer@njuics.org>
2023-01-09 20:49:05 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:49:05 up  8:26,  1 user,  load average: 0.47, 0.55, 0.53

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,6 +9,7 @@ void __am_switch(Context *c);
 
 Context* __am_irq_handle(Context *c) {
   __am_get_cur_as(c);  
+  printf("am irq handle %x\n", c->mcause);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
