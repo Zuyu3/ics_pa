c5965234c567c08ba5e59cdbaa0c1319634dc04b
tracer-ics2022 <tracer@njuics.org>
2023-01-09 17:02:24 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:02:24 up  4:40,  1 user,  load average: 0.42, 0.26, 0.21

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -58,9 +58,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       map(&pcb->as, (void *)((prog_header.p_vaddr & ~0xfff) + 4096 * i), page_alloced + 4096 * i, MMAP_READ | MMAP_WRITE);
     }
 
-    fs_read(file_id, page_alloced, prog_header.p_filesz);
+    fs_read(file_id, page_alloced + (prog_header.p_vaddr & 0xfff), prog_header.p_filesz);
     if(prog_header.p_flags & 0x2) {
-      printf("page alloced: %p, vaddr: %p, memset start offset from: %p\n", page_alloced, prog_header.p_vaddr, page_alloced + (prog_header.p_vaddr & 0xfff));
       memset(page_alloced + (prog_header.p_vaddr & 0xfff) + prog_header.p_filesz, 0, prog_header.p_memsz - prog_header.p_filesz);
       printf("loader set maxbrk: %p\n", (prog_header.p_vaddr & ~0xfff) + 4096 * page_num);
       pcb->max_brk = (prog_header.p_vaddr & ~0xfff) + 4096 * page_num;
