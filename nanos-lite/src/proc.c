54390aa6bfc4242a98dc5cda53c25bbd628dd648
tracer-ics2022 <tracer@njuics.org>
2023-01-10 21:45:16 +0800

    >  compile NEMU 211850020 王雷宇 Linux zuyu-virtual-machine 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:45:16 up 10:11,  1 user,  load average: 0.53, 0.51, 0.38

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -64,8 +64,8 @@ void init_proc() {
 Context* schedule(Context *prev) {
   // save the context pointer
   current->cp = prev;
-
-  current = (current == &pcb[0] ? &pcb[pcb_index - 1] : &pcb[0]);
+  current = &pcb[pcb_index - 1];
+  //current = (current == &pcb[0] ? &pcb[pcb_index - 1] : &pcb[0]);
 
   // then return the new context
   return current->cp;
